4d6ff4d7a96eb440d2f507cfdf635b0280c2e09a
huangpeng <275945793@qq.com>
2026-04-30 16:19:29 +0800

    upd

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
-import { Sequelize } from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import mysql from 'mysql2/promise';
+//import mysql from 'mysql2/promise';
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME!;
@@ -16,31 +16,48 @@ const DB_CONFIG = {
 };
 
 // 先创建数据库（不存在就创建）
-const initDB = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-  });
-  await connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`);
-  await connection.end();
-};
+// const initDB = async () => {
+//   const connection = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PWD,
+//   });
+//   await connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`);
+//   await connection.end();
+// };
 
-initDB();
+// initDB();
 
 // 连接数据库
+// const sequelize = new Sequelize(
+//   DB_NAME,
+//   process.env.DB_USER!,
+//   process.env.DB_PWD!,
+//   // {
+//   //   host: process.env.DB_HOST,
+//   //   dialect: 'mysql',
+//   //   logging: false
+//   // },
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     port: parseInt(process.env.DB_PORT || '5432'), // PostgreSQL 默认 5432
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false // Railway 必须加这个
+//       }
+//     },
+//     logging: false
+//   }
+// );
 const sequelize = new Sequelize(
-  DB_NAME,
+  process.env.DB_NAME!,
   process.env.DB_USER!,
   process.env.DB_PWD!,
-  // {
-  //   host: process.env.DB_HOST,
-  //   dialect: 'mysql',
-  //   logging: false
-  // },
   {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
+    host: process.env.DB_HOST!,
+    dialect: 'postgres', // 必须改成 postgres
     port: parseInt(process.env.DB_PORT || '5432'), // PostgreSQL 默认 5432
     dialectOptions: {
       ssl: {
@@ -51,5 +68,28 @@ const sequelize = new Sequelize(
     logging: false
   }
 );
+// ------------------------------
+// 定义用户表模型（自动建表）
+// ------------------------------
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// ------------------------------
+// 自动同步表结构！！！
+// 这一句 = 自动建表
+// ------------------------------
+sequelize.sync()
+  .then(() => console.log('✅ 数据库表自动创建完成！'))
+  .catch(err => console.error('❌ 同步失败：', err));
 
 export default sequelize;
+export { User };
